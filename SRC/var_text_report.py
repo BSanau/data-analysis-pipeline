394b8c12bb779a53750c0a8510e142d2314cf3af
Beatriz Sanau <beatriz.sanau@gmail.com>
2020-04-13 14:14:33 +0200

    text

---
+++ SRC/var_text_report.py
@@ -0,0 +1,7 @@
+# Text for the report
+texto = {
+    "Situation1" : "lorem ipsum Ghana Government",
+    "Situation2" : "lorem ipsum Ghana Private",
+    "Situation3" : "lorem ipsum Spain Government",
+    "Situation4" : "lorem ipsum Spain Private"
+}
