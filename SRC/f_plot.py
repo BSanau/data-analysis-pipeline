b23ba57b4da4d6fac8aefc80dae7c9c922c2402a
Beatriz Sanau <beatriz.sanau@gmail.com>
2020-04-13 14:12:25 +0200

    plot function

--- SRC/f_plot.py
+++ SRC/f_plot.py
@@ -3,24 +3,46 @@
 import matplotlib.pyplot as plt
 
 def plotfunction (df):
-    pop_hosp=df["Population (M)"]/df["Hospitals"]
-    x = df["Mortality"]/1000*10
-    y = pop_hosp
 
     country = df["Country"][0]
     owner = df["Ownership"][0]
     title = f"{country}: {owner} hospitals"
 
+    # Setting colours
     if country == "Ghana":
         colors = "#ff3333"
     elif country == "Spain":
         colors = "#333fff"
 
-    # Plot
-    plt.scatter(x, y, c=colors)
-    plt.title(title)
-    plt.xlabel('Infant Mortality (%)')
-    plt.ylabel('Poputalion (M) /hospitals')
-    plt.show()
+    # Creating two subplots
+    figure, (ax1, ax2) = plt.subplots(2,1)
 
-    #return plt
+
+    # Plot 1: Function Population(M)/Hospitals versus Mortality
+    pop_hosp=df["Population (M)"]/df["Hospitals"]
+    x = df["Mortality"]/1000*10
+    y = pop_hosp
+
+    # Plot
+    ax1.scatter(x, y, c=colors)
+    #ax1.set_xlabel('Infant Mortality (%)')
+    ax1.set_ylabel('Poputalion (M)/ Hospitals')
+
+
+    # Plot 2: Function Area/Hospitals versus Mortality
+    area_hosp=df["Area (km2)"]/df["Hospitals"]
+    x = df["Mortality"]/1000*10
+    y = area_hosp
+
+    # Plot
+    ax2.scatter(x, y, c=colors)
+    ax2.set_xlabel('Infant Mortality (%)')
+    ax2.set_ylabel('Area (km2)/ Hospitals')
+
+
+    # Title
+    figure.suptitle(title)
+
+    # Saving figure
+    plt.savefig("OUTPUT/graphic.png")
+    plt.show()
